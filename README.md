e43c99c4935399d2fcc888b90fbbdc5f2ffb46bc
Peter Mancini <petermancini@STORMTROOPER.(none)>
2013-04-22 17:38:34 -0500

    Added sanitize-string function.

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # noise-control
 
-A Clojure library designed to ... well, that part is up to you.
+A Clojure library designed to control noise in text files.
 
 ## Usage
 
-FIXME
+The suggested pipeline is to filter files sentence by sentence to see if they parse as english. If so, then sanitize-string each sentence. Save just those results.
 
 ## License
 
-Copyright © 2013 FIXME
+Copyright © 2013 Cicayda LLC
 
 Distributed under the Eclipse Public License, the same as Clojure.
--- src/noise_control/noise_control.clj
+++ src/noise_control/noise_control.clj
@@ -46,5 +46,11 @@
 (defn filter-english-only [sentences]
   (filter english? sentences))
 
-(defn character-filter [sentences]
-  )
+(defn sanitize-string [s] (clojure.string/replace (apply str (map (fn [c]
+         (if (some #{c} '(\a \b \c \d \e \f \g \h \i \j \k \l \m \n \o \p \q \r \s \t \u \v \w \x \y \z 
+                          \A \B \C \D \E \F \G \H \I \J \K \L \M \N \O \P \Q \R \S \T \U \V \W \X \Y \Z
+                          \0 \1 \2 \3 \4 \5 \6 \7 \8 \9
+                          \. \! \? \, \; \: \& \@ \[ \] \{ \} \$ \% \< \> \\ \~ \' \space))
+           c
+           \space))
+       s)) #"  +" " "))
